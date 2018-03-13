cec820e8b8334dbfcb12bebde2860d74210660e5
localsearch\olukin <oleg.lukin@localsearch.com.au>
2018-03-13 21:52:03 +1000

    Implemented cache logic

--- Cache.cs
+++ Cache.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace assignment
 {
     public class Cache<TKey, TValue> : ICache<TKey, TValue>
     {
         private int maximumNumberOfElements;
+        private List<Tuple<TKey, TValue, DateTime>> values;
 
         /// <summary>
         /// Cache constructor. Accepts maximum number of elements stored in the cache.
@@ -11,18 +15,45 @@ namespace assignment
         public Cache(int maximumNumberOfElements)
         {
             this.maximumNumberOfElements = maximumNumberOfElements;
+            this.values = new List<Tuple<TKey, TValue, DateTime>>();
         }
 
+
+        /// <summary>
+        /// Add new value to the cache or update existing one. Cache size cannot be larger than defined
+        /// value of maximumNumberOfElements. If it reaches this maximum size then it should remove 
+        /// the oldest (least recent) element.
+        /// </summary>
         public void AddOrUpdate(TKey key, TValue value)
         {
-
+            DateTime now = new DateTime();
+            var element = values.Where(e => e.Item1.Equals(key)).FirstOrDefault();
+            if (element != null)
+            {
+                values[values.FindIndex(e => e.Item1.Equals(key))] = Tuple.Create(key, value, now);
+            }
+            else if (values.Count < maximumNumberOfElements)
+            {
+                values.Add(Tuple.Create(key, value, now));
+            }
+            else
+            {
+                var minDateTime = values.Min(e => e.Item3);
+                values.Remove(values.Where(e => e.Item3.Equals(minDateTime)).First());
+                values.Add(Tuple.Create(key, value, now));
+            }
         }
-        
+
+
+        /// <summary>
+        /// Try to get cached value by key. Return true if it can find the value. Otherwise return false.
+        /// </summary>
         public bool TryGetValue(TKey key, out TValue value)
         {
-            value = default(TValue);
-            return false;
+            var element = values.Where(e => e.Item1.Equals(key)).FirstOrDefault();
+            bool found = (element != null) ? true : false;
+            value = (element != null) ? element.Item2 : default(TValue);
+            return found;
         }
     }
-
 }
--- Program.cs
+++ Program.cs
@@ -8,8 +8,36 @@ namespace assignment
         {
             int maximumNumberOfElements = 4;
             Console.WriteLine("Creating cache with maximum of {0} elements.", maximumNumberOfElements);
-            ICache<string, int> cache = new Cache<string, int>(maximumNumberOfElements);
+            ICache<string, int?> cache = new Cache<string, int?>(maximumNumberOfElements);
 
+            cache.AddOrUpdate("one", 1);
+            cache.AddOrUpdate("two", 2);
+            cache.AddOrUpdate("three", 3);
+            cache.AddOrUpdate("four", 4);
+
+            int? val = 0;
+            bool found = cache.TryGetValue("one", out val);
+            Console.WriteLine("Found value {0}: {1}", val, found);
+
+            found = cache.TryGetValue("two", out val);
+            Console.WriteLine("Found value {0}: {1}", val, found);
+
+            found = cache.TryGetValue("five", out val);
+            Console.WriteLine("Found value {0}: {1}", val, found);
+
+
+            cache.AddOrUpdate("five", 5);
+            cache.AddOrUpdate("two", 22);
+
+            found = cache.TryGetValue("one", out val);
+            Console.WriteLine("Found value {0}: {1}", val, found);
+
+            found = cache.TryGetValue("two", out val);
+            Console.WriteLine("Found value {0}: {1}", val, found);
+
+            found = cache.TryGetValue("five", out val);
+            Console.WriteLine("Found value {0}: {1}", val, found);
+            
 
             Console.WriteLine("Press Enter...");
             Console.ReadLine();
