b94a2917a383eca5665b5c6c692ee6e5b4662494
localsearch\olukin <oleg.lukin@localsearch.com.au>
2018-03-13 22:23:36 +1000

    Reorganised projects folders. added unit test project

---
+++ assignment_test/UnitTest1.cs
@@ -0,0 +1,48 @@
+using System;
+// using assignment;
+// using Xunit;
+
+namespace assignment_test
+{
+    public class UnitTest1
+    {
+        private int val;
+        // [Fact]
+        // public void Test1()
+        // {
+        //     int maximumNumberOfElements = 4;
+        //     Console.WriteLine("Creating cache with maximum of {0} elements.", maximumNumberOfElements);
+        //     ICache<string, int?> cache = new Cache<string, int?>(maximumNumberOfElements);
+
+        //     cache.AddOrUpdate("one", 1);
+        //     cache.AddOrUpdate("two", 2);
+        //     cache.AddOrUpdate("three", 3);
+        //     cache.AddOrUpdate("four", 4);
+
+        //     int? val = 0;
+        //     bool found = cache.TryGetValue("one", out val);
+        //     Assert.True(val == 1);
+        //     Assert.True(found);
+        //     Console.WriteLine("Found value {0}: {1}", val, found);
+
+        //     found = cache.TryGetValue("two", out val);
+        //     Console.WriteLine("Found value {0}: {1}", val, found);
+
+        //     found = cache.TryGetValue("five", out val);
+        //     Console.WriteLine("Found value {0}: {1}", val, found);
+
+
+        //     cache.AddOrUpdate("five", 5);
+        //     cache.AddOrUpdate("two", 22);
+
+        //     found = cache.TryGetValue("one", out val);
+        //     Console.WriteLine("Found value {0}: {1}", val, found);
+
+        //     found = cache.TryGetValue("two", out val);
+        //     Console.WriteLine("Found value {0}: {1}", val, found);
+
+        //     found = cache.TryGetValue("five", out val);
+        //     Console.WriteLine("Found value {0}: {1}", val, found);
+        // }
+    }
+}
