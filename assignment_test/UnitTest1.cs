5ec9315c4668fa898886f3b049c7d5f0516e1a5b
localsearch\olukin <oleg.lukin@localsearch.com.au>
2018-03-13 22:37:36 +1000

    Developed unit test

--- assignment_test/UnitTest1.cs
+++ assignment_test/UnitTest1.cs
@@ -1,48 +1,61 @@
 using System;
-// using assignment;
-// using Xunit;
+using assignment;
+using Xunit;
 
 namespace assignment_test
 {
     public class UnitTest1
     {
         private int val;
-        // [Fact]
-        // public void Test1()
-        // {
-        //     int maximumNumberOfElements = 4;
-        //     Console.WriteLine("Creating cache with maximum of {0} elements.", maximumNumberOfElements);
-        //     ICache<string, int?> cache = new Cache<string, int?>(maximumNumberOfElements);
+        [Fact]
+        public void Test1()
+        {
+            int maximumNumberOfElements = 4;
+            Console.WriteLine("Creating cache with maximum of {0} elements.", maximumNumberOfElements);
+            ICache<string, int?> cache = new Cache<string, int?>(maximumNumberOfElements);
 
-        //     cache.AddOrUpdate("one", 1);
-        //     cache.AddOrUpdate("two", 2);
-        //     cache.AddOrUpdate("three", 3);
-        //     cache.AddOrUpdate("four", 4);
+            // Add 4 values to the cache
+            cache.AddOrUpdate("one", 1);
+            cache.AddOrUpdate("two", 2);
+            cache.AddOrUpdate("three", 3);
+            cache.AddOrUpdate("four", 4);
 
-        //     int? val = 0;
-        //     bool found = cache.TryGetValue("one", out val);
-        //     Assert.True(val == 1);
-        //     Assert.True(found);
-        //     Console.WriteLine("Found value {0}: {1}", val, found);
+            int? val = 0;
 
-        //     found = cache.TryGetValue("two", out val);
-        //     Console.WriteLine("Found value {0}: {1}", val, found);
+            // Try to get first element from the cache, make sure it exists there
+            bool found = cache.TryGetValue("one", out val);
+            Assert.True(val == 1);
+            Assert.True(found);
 
-        //     found = cache.TryGetValue("five", out val);
-        //     Console.WriteLine("Found value {0}: {1}", val, found);
+            // Try to get second element, it also should be there
+            found = cache.TryGetValue("two", out val);
+            Assert.True(val == 2);
+            Assert.True(found);
+
+            // Try to get element that doesn't exist, make sure it's not there
+            found = cache.TryGetValue("five", out val);
+            Assert.Null(val);
+            Assert.False(found);
 
 
-        //     cache.AddOrUpdate("five", 5);
-        //     cache.AddOrUpdate("two", 22);
+            // Add one new element (5) and update an existing element (2)
+            cache.AddOrUpdate("five", 5);
+            cache.AddOrUpdate("two", 22);
 
-        //     found = cache.TryGetValue("one", out val);
-        //     Console.WriteLine("Found value {0}: {1}", val, found);
+            // Try to get value that should not longer be there as it is the oldest one (should be evicted)
+            found = cache.TryGetValue("one", out val);
+            Assert.Null(val);
+            Assert.False(found);
 
-        //     found = cache.TryGetValue("two", out val);
-        //     Console.WriteLine("Found value {0}: {1}", val, found);
+            // Try to get value that has been updated (2 => 22)
+            found = cache.TryGetValue("two", out val);
+            Assert.True(val == 22);
+            Assert.True(found);
 
-        //     found = cache.TryGetValue("five", out val);
-        //     Console.WriteLine("Found value {0}: {1}", val, found);
-        // }
+            // Try to get newly added value (5)
+            found = cache.TryGetValue("five", out val);
+            Assert.True(val == 5);
+            Assert.True(found);
+        }
     }
 }
