b5529325a1ed571dea4f3db3c54c359699e28784
localsearch\olukin <oleg.lukin@localsearch.com.au>
2018-03-13 23:08:27 +1000

    Added locking for thread sunchronisation

--- code/Cache.cs
+++ code/Cache.cs
@@ -27,20 +27,23 @@ namespace assignment
         public void AddOrUpdate(TKey key, TValue value)
         {
             DateTime now = new DateTime();
-            var element = values.Where(e => e.Item1.Equals(key)).FirstOrDefault();
-            if (element != null)
+            lock (values)
             {
-                values[values.FindIndex(e => e.Item1.Equals(key))] = Tuple.Create(key, value, now);
-            }
-            else if (values.Count < maximumNumberOfElements)
-            {
-                values.Add(Tuple.Create(key, value, now));
-            }
-            else
-            {
-                var minDateTime = values.Min(e => e.Item3);
-                values.Remove(values.Where(e => e.Item3.Equals(minDateTime)).First());
-                values.Add(Tuple.Create(key, value, now));
+                var element = values.Where(e => e.Item1.Equals(key)).FirstOrDefault();
+                if (element != null)
+                {
+                    values[values.FindIndex(e => e.Item1.Equals(key))] = Tuple.Create(key, value, now);
+                }
+                else if (values.Count < maximumNumberOfElements)
+                {
+                    values.Add(Tuple.Create(key, value, now));
+                }
+                else
+                {
+                    var minDateTime = values.Min(e => e.Item3);
+                    values.Remove(values.Where(e => e.Item3.Equals(minDateTime)).First());
+                    values.Add(Tuple.Create(key, value, now));
+                }
             }
         }
 
@@ -50,9 +53,13 @@ namespace assignment
         /// </summary>
         public bool TryGetValue(TKey key, out TValue value)
         {
-            var element = values.Where(e => e.Item1.Equals(key)).FirstOrDefault();
-            bool found = (element != null) ? true : false;
-            value = (element != null) ? element.Item2 : default(TValue);
+            bool found = false;
+            lock (values)
+            {
+                var element = values.Where(e => e.Item1.Equals(key)).FirstOrDefault();
+                found = (element != null) ? true : false;
+                value = (element != null) ? element.Item2 : default(TValue);
+            }
             return found;
         }
     }
